0856d52dee0a8c09a823767b3ff271823d6fe5a3
Santiago Aliaga Estella <santiago@MacBook-Air-de-Santiago.local>
2024-04-01 20:32:55 +0200

    try different date

--- src/etls/boe/load.py
+++ src/etls/boe/load.py
@@ -9,7 +9,7 @@ from src.etls.common.utils import TextLoader
 def today():
     boe_scrapper = BOEScrapper()
     # TODO: Quitar
-    day = date.today() - timedelta(days=5)
+    day = date.today() - timedelta(days=6)
     docs = boe_scrapper.download_day(day)
     documents = {}
     for doc in docs:
