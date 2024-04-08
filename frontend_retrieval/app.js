62a716b57d7be87636f79af075f50bb77d197368
Santiago Aliaga Estella <santiago@MacBook-Air-de-Santiago.local>
2024-04-08 19:25:14 +0200

    remove static port

--- frontend_retrieval/app.js
+++ frontend_retrieval/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 
 const app = express();
-const port = process.env.PORT || 3001
+const port = process.env.PORT
 const cors = require('cors');
 app.use(cors());
 // MongoDB connection string
